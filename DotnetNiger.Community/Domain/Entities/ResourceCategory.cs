a2f4fb534402491d3f4dde5a4a50c36af75fd600
akaletekoffilevis <l.akalete20@gmail.com>
2026-02-24 17:22:26 +0100

    fix(community): suppression du doublon

--- DotnetNiger.Community/Domain/Entities/ResourceCategory.cs
+++ DotnetNiger.Community/Domain/Entities/ResourceCategory.cs
@@ -4,12 +4,7 @@ public class ResourceCategory
 {
 	public Guid ResourceId { get; set; }
 	public Guid CategoryId { get; set; }
-	public Guid ResourceId { get; set; }
-	public Guid CategoryId { get; set; }
 
-	// FK
-	public Resource Resource { get; set; } = null!;
-	public Category Category { get; set; } = null!;
 	// FK
 	public Resource Resource { get; set; } = null!;
 	public Category Category { get; set; } = null!;
