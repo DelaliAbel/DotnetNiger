8504a988b37c6acabfb07608154e8b376d6e3b70
akaletekoffilevis <l.akalete20@gmail.com>
2026-02-24 13:14:29 +0100

    feat(identity): ajout de la troisième migration pour la base de données

---
+++ DotnetNiger.Identity/Migrations/20260221154320_TroisiemeMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotnetNiger.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class TroisiemeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
